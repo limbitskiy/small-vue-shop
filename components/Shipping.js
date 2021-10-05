8193c917211bb179ed4c01458fa2ecd61cd44dcb
limbitskiy <gagarinbrood@gmail.com>
2021-10-05 17:47:43 +0300

    change delivery form

--- components/Shipping.js
+++ components/Shipping.js
@@ -4,49 +4,22 @@ app.component('shipping', {
         `
         <section class="standalone grey-bg">
             <div class="modal-window">
-                <h3 class="subtitle">Доставка</h3>
+                <h3 class="modal-title">Оформление заказа</h3>
+                <p>Оставьте свой телефон и мы перезвоним Вам для подтверждения заказа и уточнения деталей</p>
                 <form class="delivery-form">
 
                     <div class="form-input-group">
-                        <label for="fname">Имя:</label>
+                        <label for="fname"><span class="asterisk">*</span>Имя:</label>
                         <input type="text" id="fname">
                     </div>
 
                     <div class="form-input-group">
-                        <label for="lname">Фамилия:</label>
-                        <input type="text" id="lname">
-                    </div>
-
-                    <div class="form-input-group">
-                        <label for="tel">Телефон:</label>
+                        <label for="tel"><span class="asterisk">*</span>Телефон:</label>
                         <input type="tel" id="tel">
                     </div>
 
-                    <div class="form-input-group">
-                        <label for="email">Адрес электронной почты:</label>
-                        <input type="email" id="email">
-                    </div>
-
-                    <div class="form-input-group">
-                        <label for="oblast">Область:</label>
-                        <input type="text" id="oblast">
-                    </div>
-
-                    <div class="form-input-group">
-                        <label for="gorod">Город:</label>
-                        <input type="text" id="gorod">
-                    </div>
-
-                    <div class="form-input-group">
-                        <label for="nomer-pochti">Отделение Новой Почты:</label>
-                        <input type="text" id="nomer-pochti">
-                    </div>
-
-                    <div class="form-checkbox-group">
-                        <input type="checkbox" id="call-back"><span>Перезвонить по принятию заказа?</span>
-                    </div>
                 </form>
-                <button>К оплате</button>
+                <button>Готово</button>
             </div>
         </section>`,
     data() {
